9d54947ed44dc974a866da57a1713e2f319f6ba7
John Sourvinos <johnsourvinos@hotmail.com>
2023-05-29 06:58:46 +0300

    Messages clean-up

--- FrontEnd/src/app/shared/services/message-dialog.service.ts
+++ FrontEnd/src/app/shared/services/message-dialog.service.ts
@@ -65,7 +65,6 @@ export class MessageDialogService {
     public confirmDelete(): string { return this.getDescription(this.feature, 'warning') }
     public askToRefreshCalendar(): string { return this.getDescription(this.feature, 'askToRefreshCalendar') }
     public resolutionWarning(): string { return this.getDescription(this.feature, 'resolutionWarning') }
-    public reservationFound(): string { return this.getDescription(this.feature, 'reservationFound') }
     public reservationNotFound(): string { return this.getDescription(this.feature, 'reservationNotFound') }
     public helpDialog(): string { return this.getDescription(this.feature, 'helpDialog') }
 
