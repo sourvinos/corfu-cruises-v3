519ed889f7bb8ce9e09e71d13109b700a61a4a06
John Sourvinos <johnsourvinos@hotmail.com>
2023-07-06 11:17:37 +0300

    Finishes theme change with delay

--- FrontEnd/src/app/shared/components/top-bar-wrapper/logo/logo.component.ts
+++ FrontEnd/src/app/shared/components/top-bar-wrapper/logo/logo.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
 // Custom
+import { LocalStorageService } from 'src/app/shared/services/local-storage.service'
 import { LogoService } from 'src/app/features/reservations/classes/services/logo.service'
 import { MessageLabelService } from 'src/app/shared/services/message-label.service'
 
@@ -13,14 +14,32 @@ export class LogoComponent {
 
     public feature = 'loginForm'
 
-    constructor(private logoService: LogoService, private messageLabelService: MessageLabelService) { }
+    constructor(private localStorageService: LocalStorageService, private logoService: LogoService, private messageLabelService: MessageLabelService) { }
+
+    //#region public methods
 
     public getLabel(id: string): string {
         return this.messageLabelService.getDescription(this.feature, id)
     }
 
-    public getLogo(): any {
-        return this.logoService.getLogo()
+    public doLogoTasks(color: string): any {
+        this.setLogoOpacity()
+        return this.getLogo(color)
     }
+
+    //#endregion
+
+    //#region private methods
+
+    private getLogo(color: string): any {
+        return this.logoService.getLogo(color)
+    }
+
+    private setLogoOpacity(): void {
+        document.getElementById('dark').style.opacity = this.localStorageService.getItem('theme') == 'dark' ? '1' : '0'
+        document.getElementById('light').style.opacity = this.localStorageService.getItem('theme') == 'light' ? '1' : '0'
+    }
+
+    //#endregion
 
 }
