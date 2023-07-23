1826950c0a37ae6f0c2df004dace8762a95a9da9
John Sourvinos <johnsourvinos@hotmail.com>
2023-07-23 13:19:14 +0300

    Material upgrades, Part 2

--- FrontEnd/src/app/features/users/user-interface/forgot-password/forgot-password-form.component.ts
+++ FrontEnd/src/app/features/users/user-interface/forgot-password/forgot-password-form.component.ts
@@ -23,7 +23,7 @@ export class ForgotPasswordFormComponent {
     //#region variables
 
     public feature = 'forgotPasswordForm'
-    public featureIcon = 'password'
+    public featureIcon = 'forgot-password'
     public form: FormGroup
     public icon = 'arrow_back'
     public input: InputTabStopDirective
