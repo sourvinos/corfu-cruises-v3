40af51ea9ebe3eca0c277cdac0c428211aa07c04
John Sourvinos <johnsourvinos@hotmail.com>
2023-08-24 09:32:48 +0300

    Time pickers are added in all time fields

--- FrontEnd/src/app/features/pickupPoints/classes/modules/pickupPoint.module.ts
+++ FrontEnd/src/app/features/pickupPoints/classes/modules/pickupPoint.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core'
+import { NgxMatTimepickerModule } from 'ngx-mat-timepicker'
 // Custom
 import { PickupPointFormComponent } from '../../user-interface/pickupPoint-form.component'
 import { PickupPointListComponent } from '../../user-interface/pickupPoint-list.component'
@@ -11,8 +12,9 @@ import { SharedModule } from '../../../../shared/modules/shared.module'
         PickupPointFormComponent,
     ],
     imports: [
-        SharedModule,
-        PickupPointRoutingModule
+        NgxMatTimepickerModule,
+        PickupPointRoutingModule,
+        SharedModule
     ]
 })
 
