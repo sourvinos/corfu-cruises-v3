f8209bf3a5ef4ad11b4b2e5cde8480862673436e
John Sourvinos <johnsourvinos@hotmail.com>
2023-08-30 09:06:51 +0300

    Revert "Time pickers are added in all time fields"
    This reverts commit 40af51ea9ebe3eca0c277cdac0c428211aa07c04.

--- FrontEnd/src/app/features/pickupPoints/classes/modules/pickupPoint.module.ts
+++ FrontEnd/src/app/features/pickupPoints/classes/modules/pickupPoint.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core'
-import { NgxMatTimepickerModule } from 'ngx-mat-timepicker'
 // Custom
 import { PickupPointFormComponent } from '../../user-interface/pickupPoint-form.component'
 import { PickupPointListComponent } from '../../user-interface/pickupPoint-list.component'
@@ -12,9 +11,8 @@ import { SharedModule } from '../../../../shared/modules/shared.module'
         PickupPointFormComponent,
     ],
     imports: [
-        NgxMatTimepickerModule,
-        PickupPointRoutingModule,
-        SharedModule
+        SharedModule,
+        PickupPointRoutingModule
     ]
 })
 
