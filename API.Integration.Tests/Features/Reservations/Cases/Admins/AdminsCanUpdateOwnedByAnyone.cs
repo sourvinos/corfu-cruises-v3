b86590616ae9121e6598a8945678c79109fd0be9
John Sourvinos <johnsourvinos@hotmail.com>
2023-09-27 18:42:16 +0300

    Integration test for updating reservation with new portId

--- API.Integration.Tests/Features/Reservations/Cases/Admins/AdminsCanUpdateOwnedByAnyone.cs
+++ API.Integration.Tests/Features/Reservations/Cases/Admins/AdminsCanUpdateOwnedByAnyone.cs
@@ -9,12 +9,11 @@ namespace Reservations {
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
         public IEnumerator<object[]> GetEnumerator() {
-            yield return Admins_Can_Update_Own_Records();
-            yield return Admins_Can_Update_Records_Owned_By_Other_Admins();
-            yield return Admins_Can_Update_Records_Owned_By_Simple_Users();
+            yield return Admins_Can_Update();
+            yield return Admins_Can_Update_When_PortId_Is_Different_Than_Default();
         }
 
-        private static object[] Admins_Can_Update_Own_Records() {
+        private static object[] Admins_Can_Update() {
             return new object[] {
                 new TestUpdateReservation {
                     ReservationId = Guid.Parse("08da2863-15d9-4338-81fa-637a52371163"),
@@ -30,7 +29,7 @@ namespace Reservations {
             };
         }
 
-        private static object[] Admins_Can_Update_Records_Owned_By_Other_Admins() {
+        private static object[] Admins_Can_Update_When_PortId_Is_Different_Than_Default() {
             return new object[] {
                 new TestUpdateReservation {
                     ReservationId = Guid.Parse("08da2865-d8c0-40de-815c-eba6f09db081"),
@@ -39,21 +38,6 @@ namespace Reservations {
                     DestinationId = 1,
                     PickupPointId = 266,
                     TicketNo = "23",
-                    PutAt = "2023-09-07 09:55:16"
-                }
-            };
-        }
-
-        private static object[] Admins_Can_Update_Records_Owned_By_Simple_Users() {
-            return new object[] {
-                new TestUpdateReservation {
-                    ReservationId = Guid.Parse("08da30b7-fdca-4285-8fae-789dc3037124"),
-                    Date = "2022-12-04",
-                    CustomerId = 2,
-                    DestinationId = 1,
-                    PickupPointId = 129,
-                    RefNo = "PA623",
-                    TicketNo = "1258",
                     PutAt = "2023-09-07 09:55:16"
                 }
             };
