bdbb69b8c2c1267bcb31e62f62dbefc4210a1637
John Sourvinos <johnsourvinos@hotmail.com>
2024-06-05 05:58:11 +0300

    Fixes customer multiple emails validation

--- API/Features/Reservations/Customers/Validators/CustomerValidator.cs
+++ API/Features/Reservations/Customers/Validators/CustomerValidator.cs
@@ -24,7 +24,7 @@ namespace API.Features.Reservations.Customers {
             RuleFor(x => x.City).NotEmpty().MaximumLength(128);
             RuleFor(x => x.PersonInCharge).MaximumLength(128);
             RuleFor(x => x.Phones).MaximumLength(128);
-            RuleFor(x => x.Email).Must(EmailHelpers.BeEmptyOrValidEmailAddress).MaximumLength(128);
+            RuleFor(x => x.Email).MaximumLength(128);
             RuleFor(x => x.BalanceLimit).InclusiveBetween(0, 99999);
             RuleFor(x => x.Remarks).MaximumLength(2048);
         }
